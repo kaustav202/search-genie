b34e8ee9f41d9618973e5a1b511f9007da643236
kaustav202 <21f2000656@student.onlinedegree.iitm.ac.in>
2024-06-02 20:02:08 +0530

    handlers for storing and cosine similarity query on embeddings

--- db-interface/dbclient.js
+++ db-interface/dbclient.js
@@ -1,5 +1,6 @@
 
-const { Client } = require('pg');
+import pkg from 'pg';
+const {Client} = pkg;
 
 
 const client = new Client({
@@ -11,14 +12,15 @@ const client = new Client({
 });
 
 
-client.connect()
-  .then(() => {
+export default async function instertEmbeddings(text, embeddings){
+
+    let em = JSON.stringify(embeddings);
+
+    client.connect()
+   .then(() => {
     console.log('Connected to the database successfully.');
-    
 
-    const query = 'SELECT * FROM test';
-  
-
+    const query = `insert into articles(text, embedding) VALUES(${text} , ${em});`
     return client.query(query);
   })
   .then((result) => {
@@ -38,3 +40,36 @@ client.connect()
         console.error('Error closing connection', err.stack);
       });
   });
+
+}
+
+
+export async function query(embeddings){
+
+    let em = JSON.stringify(embeddings);
+
+    client.connect()
+    .then(() => {
+     console.log('Connected to the database successfully.');
+ 
+     const query = `SELECT text,  1 - (embedding <=> ${em}) AS cosine_similarity FROM Articles ORDER BY cosine_similarity desc LIMIT 5;`
+     return client.query(query);
+   })
+   .then((result) => {
+ 
+     console.log('Query result:', result.rows);
+   })
+   .catch((err) => {
+ 
+     console.error('Error executing query', err.stack);
+   })
+   .finally(() => {
+     client.end()
+       .then(() => {
+         console.log('Database connection closed.');
+       })
+       .catch((err) => {
+         console.error('Error closing connection', err.stack);
+       });
+   });
+}
