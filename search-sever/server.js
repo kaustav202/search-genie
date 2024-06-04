0090d0e1cd7c349de8102345da540eede915c712
kaustav202 <21f2000656@student.onlinedegree.iitm.ac.in>
2024-06-04 08:26:09 +0530

    search server ( direct search )

--- search-sever/server.js
+++ search-sever/server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import cors from "cors";
 
+import instertEmbeddings from "./db-interface/dbclient.js";
+import { querySimilarity } from "./db-interface/dbclient.js";
+
+import genEmbeddings from "./modelsAPI/getIndex.js";
+import { genEmbeddingsQuery } from "./modelsAPI/getIndex.js";
+
 const app = express();
 
 app.use(express.json());
@@ -12,15 +18,45 @@ app.post('/process/:s_id', async (req,res)=>{
     const {text} = req.body;
     const sessionId = req.params.s_id;
 
-    res.status(200).send('ingestion success');
+    // console.log(text);
+    console.log(sessionId);
+
+    if(sessionId == 0){
+
+        try{
+            const dataEmbeddings = await genEmbeddings(text);
+
+            await instertEmbeddings(text, dataEmbeddings);
+
+            res.status(200).send("Data Ingestion Success");
+
+        }catch{
+            console.log('Could Not Resolve Model Inference or DB Handlers');
+        }
+
+    }else{
+
+        res.status(400).send("Invalid Session!");
+    }
+
 })
 
-app.get('/search', async (req, res)=>{
+app.post('/search', async (req, res)=>{
 
     const {sTerm} = req.body;
 
+    try{
 
-    res.status(200).send('okay');
+        const quesEmbeddings = await genEmbeddingsQuery(sTerm);
+        console.log(quesEmbeddings);
+        const ans =  await querySimilarity(quesEmbeddings);
+
+        res.status(200).send(ans);
+    }catch(err){
+        console.log('Could Not Resolve Model Inference or DB Handlers');
+        console.log(err);
+    }
+
 })
 
 const port = 3000;
